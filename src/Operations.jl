4078b9c0749fceb663f3bf063d84ff997ec584ca
Artem Pelenitsyn <a.pelenitsyn@gmail.com>
2023-07-17 18:38:32 +0200

    try-update-gen-test-code

--- src/Operations.jl
+++ src/Operations.jl
@@ -1458,16 +1458,76 @@ function free(ctx::Context, pkgs::Vector{PackageSpec}; err_if_free=true)
     end
 end
 
+#
+# Patched to accommodate Stability.jl
+#
 function gen_test_code(testfile::String;
         coverage=false,
         julia_args::Cmd=``,
         test_args::Cmd=``)
     code = """
+        #### Prepare env (standard)
+        #
+        push!(LOAD_PATH, "@")
+        push!(LOAD_PATH, "@v#.#")
+        push!(LOAD_PATH, "@stdlib")
         $(Base.load_path_setup_code(false))
         cd($(repr(dirname(testfile))))
         append!(empty!(ARGS), $(repr(test_args.exec)))
-        include($(repr(testfile)))
+
+        #### Prepare Stability.jl
+        #
+        stability_root = dirname("/home/artem/stability/repo/Stability")
+        push!(LOAD_PATH, stability_root)
+        using Stability
+        pakg=ENV["STAB_PKG_NAME"]
+        wdir=ENV["WORK_DIR"]
+        #### End
+
+        #### Run tests (standard + try-catch)
+        #
+        try
+          @info "[Stability] [Package: " * pakg * "] Hooks are on. About to start testing"
+          include($(repr(testfile)))
+          @info "[Stability] [Package: " * pakg * "] Testing is finished successfully"
+        catch error
+          println("Warning: Error when running tests for package " * pakg)
+        end
+
+        #### Run Stability Analysis:
+        #
+        @info "[Stability] [Package: " * pakg * "] About to start analysis"
+        m = eval(Symbol(pakg)) # typeof(m) is Module
+        open(joinpath(wdir, "stability-errors.out"), "w") do err
+          ms = module_stats(m, err, wdir)
+          @info "[Stability] [Package: " * pakg * "] Computed module stats"
+
+          summary = modstats_summary(ms)
+          @info "[Stability] [Package: " * pakg * "] Computed module summary. About to store results in a file"
+          open(out -> println(out, pakg * "," * show_comma_sep(summary)), joinpath(wdir, "stability-summary.out"), "w")
+
+          @info "[Stability] [Package: " * pakg * "] Constructing a table from the stats..."
+          (methst, mist, tyst) = modstats_table(ms)
+
+          @info "[Stability] [Package: " * pakg * "] Table size (per method): " * string(length(methst))
+          outf = joinpath(wdir, "stability-stats-per-method.txt")
+          @info "[Stability] [Package: " * pakg * "] About to store per method results to: " * outf
+          open(f-> println(f,methst), outf,"w")
+
+          @info "[Stability] [Package: " * pakg * "] Table size (per instance): " * string(length(mist))
+          outf = joinpath(wdir, "stability-stats-per-instance.txt")
+          @info "[Stability] [Package: " * pakg * "] About to store per instance results to: " * outf
+          open(f-> println(f,mist), outf,"w")
+
+          @info "[Stability] [Package: " * pakg * "] Table size (types): " * string(length(tyst))
+          outf = joinpath(wdir, "stability-stats-intypes.txt")
+          @info "[Stability] [Package: " * pakg * "] About to store intypes to: " * outf
+          open(f-> println(f,tyst), outf,"w")
+        end
+        @info "[Stability] [Package: " * pakg * "] Finish testing + analysis"
+        #### End
         """
+    @debug code
     return ```
         $(Base.julia_cmd())
         --code-coverage=$(coverage ? "user" : "none")
@@ -1478,7 +1538,6 @@ function gen_test_code(testfile::String;
         --inline=$(Bool(Base.JLOptions().can_inline) ? "yes" : "no")
         --startup-file=$(Base.JLOptions().startupfile == 1 ? "yes" : "no")
         --track-allocation=$(("none", "user", "all")[Base.JLOptions().malloc_log + 1])
-        --threads=$(Threads.nthreads())
         $(julia_args)
         --eval $(code)
     ```
